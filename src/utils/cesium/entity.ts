6cbb2b9e06b35ce7990225323e502ae8c4e8632c
zhangyufeng <2388715231@qq.com>
2024-08-08 16:06:33 +0800

    feat:反选遮罩

--- src/utils/cesium/entity.ts
+++ src/utils/cesium/entity.ts
@@ -10,7 +10,10 @@ export function addEntity(viewer: Cesium.Viewer) {
   // line.polyline!.material = new Cesium.PolylineArrowMaterialProperty(
   //   Cesium.Color.CYAN
   // );
-  addPolygon(viewer, [117, 30,12, 117, 32,12, 116, 32,12, 116, 30,12,117, 30,12]);
+  addPolygon(
+    viewer,
+    [117, 30, 12, 117, 32, 12, 116, 32, 12, 116, 30, 12, 117, 30, 12]
+  );
   addRectangle(viewer, [114.0, 30.0, 115.0, 35.0]);
   addEllipse(viewer, { coordinates: [117, 27] });
   addCylinder(viewer, {
@@ -26,27 +29,35 @@ export function addEntity(viewer: Cesium.Viewer) {
   });
   addBox(viewer, { coordinates: [105, 35, 200000.0] });
   addEllipsoid(viewer);
-  const model=addModel(viewer,[118, 30, 5000]);
+  const model = addModel(viewer, [118, 30, 5000]);
   const degree = 90;
   const heading = Cesium.Math.toRadians(degree); //模型航向
   const pitch = 0; //俯仰角
   const roll = 0; //翻滚角
   const hpr = new Cesium.HeadingPitchRoll(heading, pitch, roll);
-  model.orientation=new Cesium.ConstantProperty(Cesium.Transforms.headingPitchRollQuaternion(Cesium.
-    Cartesian3.fromDegrees(118, 30, 5000), hpr));
-  model.show=true
+  model.orientation = new Cesium.ConstantProperty(
+    Cesium.Transforms.headingPitchRollQuaternion(
+      Cesium.Cartesian3.fromDegrees(118, 30, 5000),
+      hpr
+    )
+  );
+  model.show = true;
   addBillboard(viewer);
 }
 // 加点
 export function addPoint(
   viewer: Cesium.Viewer,
-  coordinates: [number, number, number]|Cesium.Cartesian3
+  coordinates: [number, number, number] | Cesium.Cartesian3
 ) {
   const point = new Cesium.Entity({
     // id: "point", //唯一标识符
     name: "点",
     show: true, //控制是否显示
-    position: (coordinates as Cesium.Cartesian3) ||Cesium.Cartesian3.fromDegrees(...(coordinates as [number, number, number])), //位置，需要将坐标转为笛卡尔直角坐标
+    position:
+      (coordinates as Cesium.Cartesian3) ||
+      Cesium.Cartesian3.fromDegrees(
+        ...(coordinates as [number, number, number])
+      ), //位置，需要将坐标转为笛卡尔直角坐标
     point: {
       color: Cesium.Color.GOLD, //颜色
       pixelSize: 10, //大小
@@ -83,8 +94,8 @@ export function addPolygon(viewer: Cesium.Viewer, coordinates: number[]) {
     show: true,
     polygon: {
       hierarchy: Cesium.Cartesian3.fromDegreesArrayHeights(coordinates),
-      height:12,
-      extrudedHeight:12000,
+      height: 12,
+      extrudedHeight: 12000,
       material: Cesium.Color.RED,
       // 设置贴地，另外Cesium.ClassificationType.TERRAIN表示仅贴合地形
       // classificationType: Cesium.ClassificationType.BOTH, //既可以贴合地形也可以贴合 3D Tiles
@@ -262,12 +273,19 @@ export function addEllipsoid(viewer: Cesium.Viewer) {
   return ellipsoid;
 }
 // 加小车模型glb
-export function addModel(viewer: Cesium.Viewer,coordinates: [number, number, number]|Cesium.Cartesian3) {
+export function addModel(
+  viewer: Cesium.Viewer,
+  coordinates: [number, number, number] | Cesium.Cartesian3
+) {
   const model = new Cesium.Entity({
     // id: "model", //id 唯一
     name: "小车模型", //名称
     show: true, //显示
-    position: (coordinates as Cesium.Cartesian3) ||Cesium.Cartesian3.fromDegrees(...(coordinates as [number, number, number])),//小车位置
+    position:
+      (coordinates as Cesium.Cartesian3) ||
+      Cesium.Cartesian3.fromDegrees(
+        ...(coordinates as [number, number, number])
+      ), //小车位置
     model: {
       uri: "../../../src/assets/models/car.glb",
       minimumPixelSize: 600, //模型最小
@@ -285,9 +303,9 @@ export function addBillboard(viewer: Cesium.Viewer) {
   const billboard = {
     id: "billboard",
     name: "广告牌",
-    properties:{
-      name:'人',
-      type:"billboard"
+    properties: {
+      name: "人",
+      type: "billboard",
     },
     show: true,
     position: Cesium.Cartesian3.fromDegrees(108, 30, 50),
@@ -299,4 +317,47 @@ export function addBillboard(viewer: Cesium.Viewer) {
   };
   viewer.entities.add(billboard);
   return billboard;
+}
+//反选遮罩
+//先绘制一个多边形区域，然后将需要突出的目标区域挖空，并对其余部分设置一定的透明度进行掩盖
+export function drawMask(viewer: Cesium.Viewer) {
+  //大区域的坐标点数组，够用即可，不必太大，要形成闭环
+  var pointArr1 = [
+    114.3944, 30.5237, 114.3943, 30.5192, 114.4029, 30.5192, 114.4029, 30.5237,
+    114.3944, 30.5237,
+  ];
+  var positions = Cesium.Cartesian3.fromDegreesArray(pointArr1);
+  //洞的坐标点数组，要形成闭环
+  var pointArr2 = [
+    114.3972, 30.5224, 114.3972, 30.5218, 114.3988, 30.5218, 114.3988, 30.5224,
+    114.3972, 30.5224,
+  ];
+  var hole = Cesium.Cartesian3.fromDegreesArray(pointArr2);
+  //绘制反选遮罩层
+  //带洞区域
+  viewer.entities.add({
+    name: "pol",
+    polygon: {
+      //获取指定属性 positions 和 holes（图形内需要挖空的区域）
+      hierarchy: new Cesium.PolygonHierarchy(positions, [
+        new Cesium.PolygonHierarchy(hole),
+      ]),
+      //填充的颜色，withAlpha 透明度
+      material: Cesium.Color.BLACK.withAlpha(0.7),
+      //是否被提供的材质填充
+      fill: true,
+    },
+  });
+  viewer.entities.add({
+    polyline: {
+      positions: hole,
+      width: 3,
+      material: Cesium.Color.AQUA.withAlpha(1),
+      clampToGround: true,
+    },
+  });
+  //设置相机视角
+  viewer.camera.setView({
+    destination: Cesium.Cartesian3.fromDegrees(114.3981, 30.5221, 300),
+  });
 }
